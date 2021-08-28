c46139a385057c1fe3c2e3c90732a97f625be6ad
Ryan Teixeira <ryan.m.tex@gmail.com>
2021-08-28 15:37:08 -0400

    initial commit

--- A3_Guess_A_Number/src/Guess_GameA_3.java
+++ A3_Guess_A_Number/src/Guess_GameA_3.java
@@ -4,7 +4,7 @@ Program: Information systems engineering
 
 Designed a guessing game in which the user guesses a number between 1 - 100
 The game allows the user to quit at any time by entering a negative value and play again.
-A method is used to store the guessing game.*/
+A method is used to store the guessing game. */
 
 import java.util.Scanner;
 
